e334d57d3c1971cf847cacbcca7ba29224c05588
asxiuxiu <licheng1996121@gmail.com>
2026-06-20 15:57:27 +0800

    Initialize Quartz v5 project branch

--- workspace/temp_compile/cpp-check/C++11 内存模型与 happens-before_2.cpp
+++
@@ -1,5 +0,0 @@
-int main() {
-    int a = 1;
-    int b = a + 2;  // a = 1 sequenced-before b = 3
-    return 0;
-}
--- workspace/temp_compile/cpp-check/原子操作与无锁编程基础_0.cpp
+++
@@ -1,9 +0,0 @@
-#include <thread>
-
-volatile int counter = 0;
-
-void worker() {
-    for (int i = 0; i < 100000; ++i) {
-        ++counter;  // 仍然不是原子操作！
-    }
-}
